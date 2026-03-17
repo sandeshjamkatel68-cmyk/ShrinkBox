0c7b399f577f9b5cb0c798085ea12ecbc50fd42b
Sandesh Jamkatel <sandeshjamkatel68@gmail.com>
2026-03-17 19:23:15 +0545

    commited just made by adding the google adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -83,6 +83,8 @@ export default function RootLayout({
             gtag('config', 'G-WR9QK5Q510');
           `}
         </Script>
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3220445637759521"
+     crossorigin="anonymous"></script>
       </head>
 
       <body className="min-h-screen flex flex-col">
